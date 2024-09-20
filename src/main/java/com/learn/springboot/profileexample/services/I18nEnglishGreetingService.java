affa5c3db9fe99e3a3ed9f14cc774aec84744940
arunchauhan <arunchauhan578@gmail.com>
2024-09-20 13:03:44 +0530

    Qualifier Explanation with example

--- src/main/java/com/learn/springboot/profileexample/services/I18nEnglishGreetingService.java
+++ src/main/java/com/learn/springboot/profileexample/services/I18nEnglishGreetingService.java
@@ -3,7 +3,8 @@ package com.learn.springboot.profileexample.services;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.*;
 
-@Profile("EN")
+//@Profile("EN")
+@Profile({"EN", "default"})     //Whenever there is no active bean setup in application.properties then default bean would pick up
 @Service("i18nService") //so now spring managed bean reference name would be "i18nService"
 public class I18nEnglishGreetingService implements GreetingService {
     @Override
